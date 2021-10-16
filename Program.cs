87ad5fb6010eac901743db9d8f93f1ec3c3e765c
vishal.Alok <you@example.com>
2021-10-16 07:09:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+sealed public class Animal
+{
+    public void eat() { Console.WriteLine("eating..."); }
+}
+public class Dog : Animal //NOT ALLOWED
+{
+    public void bark() { Console.WriteLine("barking..."); }
+}
+public class TestSealed
+{
+    public static void Main()
+    {
+        Dog d = new Dog();
+        d.eat(); //NOT ALLOWED
+        d.bark();
+    }
+}
